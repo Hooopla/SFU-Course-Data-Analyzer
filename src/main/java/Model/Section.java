92ddd6b04bb6e951bb5e57d42885fa742441b825
Gabriel Bello <johngabrielbello@outlook.com>
2024-03-28 20:40:02 -0700

    Update Section.java

--- src/main/java/Model/Section.java
+++ src/main/java/Model/Section.java
@@ -1,4 +1,27 @@
 package Model;
 
 public class Section {
+
+    private String type;
+    private int enrollmentMax;
+    private int enrollmentTotal;
+
+    public Section(CourseData data) {
+        this.type = data.getComponent();
+        this.enrollmentMax = data.getEnrollmentCap();
+        this.enrollmentTotal = data.getEnrollmentTotal();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getEnrollmentMax() {
+        return enrollmentMax;
+    }
+
+    public int getEnrollmentTotal() {
+        return enrollmentTotal;
+    }
+
 }
