96e7ab403993a55e83a58a872c8b691f6bd6c590
Gabriel Bello <johngabrielbello@outlook.com>
2024-04-14 19:33:32 -0700

    Added a general guideline
    Small Changes
    
    - Created a Authors class to ensure clarity instead of "API" class. I thought it was kind of a poorly named class.

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,9 +1,6 @@
 package Controller;
 
-import Model.CourseOfferings;
-import Model.Department;
-import Model.DumpWriter;
-import Model.ModelLoader;
+import Model.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +12,54 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class Controller {
+    private List<Department> departmentList = new ArrayList<>();
+    private Authors authors = new Authors();
+
+    // About Us Page | @GetMapping
+    @GetMapping("/about")
+    public Authors getAboutPage() {
+        return authors;
+    }
+
+
+    // Get List of Departments | @GetMapping
+    @GetMapping("/departments")
+    public List<Department> getDepartmentList() {
+        return departmentList;
+    }
+    // Get One Specific Department | @GetMapping
+    // Comment:
+
+    // Get Total Amount of Students in that department each Semester | @GetMapping
+    // Comment: I need to watch the video again as I am not sure if this is going to be used in the graph or not.
+
+    // Get List of Course | @GetMapping
+    // Comment:
+
+    // Get One Specific Course | @GetMapping
+    // Comment:
+
+    // Get List of Course Offering | @GetMapping
+    // Comment:
+
+    // Get One Specific Course Offering | @GetMapping
+    // Comment:
+
+
+    // Get List of Sections @GetMapping | @GetMapping
+    // Comment:
+    
+    // Get One Specific Section @GetMapping | @GetMapping
+    // Comment:
+
+    // HELLO ALEX IF U SEE THIS ANYTHING BELOW THIS IS FUNCTION THAT WE MAY NEED TO MAKE? I just need to rewatch the video or if u know u can get rid of it as needed.
+    // AddCourseOffering??
+    // AddCourse??
+    // AddSection??
+
+    // Get Dump Model | @GetMapping
     @GetMapping("dump-model")
     private void printToConsole() {
-        List<Department> departmentList = new ArrayList<>();
         ModelLoader loader = new ModelLoader("data/course_data_2018.csv", departmentList);
         DumpWriter.dumpModel(departmentList);
     }
---
+++ src/main/java/Model/Authors.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class Authors {
+    private String appName = "Course Planner Controller.Application";
+    private String authorName = "Gabriel B. & Alex V.";
+
+    public Authors() {
+    }
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
